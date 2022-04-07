d76eb7b06c8eccf30e4e3b0ead7d5ffb8a01bc6a
Rosen Toshev <rtoscheff@gmail.com>
2022-04-07 17:59:48 +0100

    Initial commit

---
+++ App.js
@@ -0,0 +1,113 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, {useState} from 'react';
+import {StyleSheet, View, Text, Button, Platform} from 'react-native';
+const axios = require('axios').default;
+
+const URL = 'https://busdue.com';
+
+const App = () => {
+  const [validationMsg, setValidationMsg] = useState('Waiting');
+  const [validationStatus, setValidationStatus] = useState('');
+  const onConnectPress = () => {
+    fetch(URL)
+      .then(res => {
+        console.log('**************');
+        console.log(res);
+        console.log('**************');
+        setValidationMsg('Valid certificate, connected.');
+        setValidationStatus('success');
+      })
+      .catch(() => {
+        setValidationMsg('Certificate does not match, connection refused');
+        setValidationStatus('failed');
+      });
+  };
+
+  const onAxiosCertFetch = () => {
+    axios
+      .get(URL)
+      .then(res => {
+        console.log('**************');
+        console.log(res);
+        console.log('**************');
+        setValidationMsg('Valid certificate, connected.');
+        setValidationStatus('success');
+      })
+      .catch(() => {
+        setValidationMsg('Certificate does not match, connection refused');
+        setValidationStatus('failed');
+      });
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>React Native SSL Pinning</Text>
+      <Text style={styles.title}>({Platform.OS.toUpperCase()})</Text>
+      <Text style={styles.header}>Certificate status:</Text>
+      <Text
+        style={[
+          styles.status,
+          validationStatus === 'success' && styles.success,
+          validationStatus === 'failed' && styles.failed,
+        ]}>
+        {validationMsg}
+      </Text>
+      <View style={styles.btnContainer}>
+        <Button title={`Test fetch call: ${URL}`} onPress={onConnectPress} />
+      </View>
+      <View style={styles.btnContainer}>
+        <Button title={`Test axios call: ${URL}`} onPress={onAxiosCertFetch} />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 24,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  header: {
+    paddingTop: 46,
+    fontSize: 18,
+    textAlign: 'center',
+  },
+  status: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  success: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'green',
+  },
+  failed: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'red',
+  },
+  btnContainer: {
+    borderWidth: 1,
+    borderColor: 'grey',
+    borderRadius: 4,
+    paddingHorizontal: 16,
+    marginTop: 24,
+  },
+});
+
+export default App;
---
+++ ios/sslPinningFetch/AppDelegate.mm
@@ -0,0 +1,137 @@
+#import "AppDelegate.h"
+
+#import <React/RCTBridge.h>
+#import <React/RCTBundleURLProvider.h>
+#import <React/RCTRootView.h>
+
+#import <React/RCTAppSetupUtils.h>
+
+#import <TrustKit/TrustKit.h>
+#import <TrustKit/TSKPinningValidator.h>
+#import <TrustKit/TSKPinningValidatorCallback.h>
+
+#if RCT_NEW_ARCH_ENABLED
+#import <React/CoreModulesPlugins.h>
+#import <React/RCTCxxBridgeDelegate.h>
+#import <React/RCTFabricSurfaceHostingProxyRootView.h>
+#import <React/RCTSurfacePresenter.h>
+#import <React/RCTSurfacePresenterBridgeAdapter.h>
+#import <ReactCommon/RCTTurboModuleManager.h>
+
+
+#import <react/config/ReactNativeConfig.h>
+
+@interface AppDelegate () <RCTCxxBridgeDelegate, RCTTurboModuleManagerDelegate> {
+  RCTTurboModuleManager *_turboModuleManager;
+  RCTSurfacePresenterBridgeAdapter *_bridgeAdapter;
+  std::shared_ptr<const facebook::react::ReactNativeConfig> _reactNativeConfig;
+  facebook::react::ContextContainer::Shared _contextContainer;
+}
+@end
+#endif
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+  RCTAppSetupPrepareApp(application);
+
+  NSDictionary *trustKitConfig =
+@{
+  // Swizzling because we can't access the NSURLSession instance used in React Native's fetch method
+
+  kTSKSwizzleNetworkDelegates: @YES,
+  kTSKPinnedDomains: @{
+      @"busdue.com" : @{
+          kTSKIncludeSubdomains: @YES, // Pin all subdomains
+          kTSKEnforcePinning: @YES, // Block connections if pinning validation failed
+          kTSKDisableDefaultReportUri: @YES,
+          kTSKPublicKeyHashes : @[
+            @"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=",
+            @"BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB=", // Fake backup key but we need to provide 2 pins
+          ],
+      },
+  }};
+[TrustKit initSharedInstanceWithConfiguration:trustKitConfig];
+[TrustKit sharedInstance].pinningValidatorCallback = ^(TSKPinningValidatorResult *result, NSString *notedHostname, TKSDomainPinningPolicy *policy) {
+  if (result.finalTrustDecision == TSKTrustEvaluationFailedNoMatchingPin) {
+    NSLog(@"TrustKit certificate matching failed");
+    // Add more logging here. i.e. Sentry, BugSnag etc
+  }
+};
+
+  RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
+
+#if RCT_NEW_ARCH_ENABLED
+  _contextContainer = std::make_shared<facebook::react::ContextContainer const>();
+  _reactNativeConfig = std::make_shared<facebook::react::EmptyReactNativeConfig const>();
+  _contextContainer->insert("ReactNativeConfig", _reactNativeConfig);
+  _bridgeAdapter = [[RCTSurfacePresenterBridgeAdapter alloc] initWithBridge:bridge contextContainer:_contextContainer];
+  bridge.surfacePresenter = _bridgeAdapter.surfacePresenter;
+#endif
+
+  UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"sslPinningFetch", nil);
+
+  if (@available(iOS 13.0, *)) {
+    rootView.backgroundColor = [UIColor systemBackgroundColor];
+  } else {
+    rootView.backgroundColor = [UIColor whiteColor];
+  }
+
+  self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+  UIViewController *rootViewController = [UIViewController new];
+  rootViewController.view = rootView;
+  self.window.rootViewController = rootViewController;
+  [self.window makeKeyAndVisible];
+  return YES;
+}
+
+- (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
+{
+#if DEBUG
+  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
+#else
+  return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+#endif
+}
+
+#if RCT_NEW_ARCH_ENABLED
+
+#pragma mark - RCTCxxBridgeDelegate
+
+- (std::unique_ptr<facebook::react::JSExecutorFactory>)jsExecutorFactoryForBridge:(RCTBridge *)bridge
+{
+  _turboModuleManager = [[RCTTurboModuleManager alloc] initWithBridge:bridge
+                                                             delegate:self
+                                                            jsInvoker:bridge.jsCallInvoker];
+  return RCTAppSetupDefaultJsExecutorFactory(bridge, _turboModuleManager);
+}
+
+#pragma mark RCTTurboModuleManagerDelegate
+
+- (Class)getModuleClassFromName:(const char *)name
+{
+  return RCTCoreModulesClassProvider(name);
+}
+
+- (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:(const std::string &)name
+                                                      jsInvoker:(std::shared_ptr<facebook::react::CallInvoker>)jsInvoker
+{
+  return nullptr;
+}
+
+- (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:(const std::string &)name
+                                                     initParams:
+                                                         (const facebook::react::ObjCTurboModule::InitParams &)params
+{
+  return nullptr;
+}
+
+- (id<RCTTurboModule>)getModuleInstanceFromClass:(Class)moduleClass
+{
+  return RCTAppSetupDefaultModuleFromClass(moduleClass);
+}
+
+#endif
+
+@end
